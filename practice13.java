3daf7dca7890144b265d258479b78cbcf53757f8
1160300526 <1181049074@qq.com>
2020-02-27 11:59:42 +0800

    try hashmap

--- practice13.java
+++ practice13.java
@@ -1,12 +1,20 @@
-
+import java.util.HashMap;
 public class practice13 {
 	public int romanToInt(String s) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
         int len = s.length();
         int res = 0;
         int tem0 = 1000;
         int tem1 = 0;
         for (int i=0; i<len; i++) {
-        	tem1 = this.getValue(s.charAt(i));
+        	tem1 = map.get(s.charAt(i));
         	if (tem0 >= tem1) res = res + tem1;
         	else res = res + tem1 - 2 * tem0;
         	tem0 = tem1;
@@ -14,40 +22,10 @@ public class practice13 {
         return res;
     }
 
-	public int getValue(char c) {
-		int ans = 0;
-		switch (c) {
-    	case 'I':
-    		ans = 1;
-    		break;
-    	case 'V':
-    		ans = 5;
-    		break;
-    	case 'X':
-    		ans = 10;
-    		break;
-    	case 'L':
-    		ans = 50;
-    		break;
-    	case 'C':
-    		ans = 100;
-    		break;
-    	case 'D':
-    		ans = 500;
-    		break;
-    	case 'M':
-    		ans = 1000;
-    		break;
-    	default:
-    		System.out.println("wrong character");
-
-    	}
-		return ans;
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		practice13 p = new practice13();
-		System.out.println(p.romanToInt("MDCLXVI"));
+		System.out.println(p.romanToInt("CMXCIX"));
 	}
 
 }
