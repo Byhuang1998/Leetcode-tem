2a2a3a58f3b6dad3836b4570666d6d3f7ae9f243
1160300526 <1181049074@qq.com>
2020-02-27 16:46:00 +0800

    longestCommonPrefix

--- practice14.java
+++ practice14.java
@@ -6,12 +6,12 @@ public class practice14 {
 		   String prefix = strs[0];
 		   for (int i = 1; i < strs.length; i++)
 		       while (strs[i].indexOf(prefix) != 0) {
+		    	   // 如果结果不是0，意味着prefix没有匹配上，于是下一条语句是：截取掉最末尾的一个字母，再匹配一次
 		           prefix = prefix.substring(0, prefix.length() - 1);
 		           if (prefix.isEmpty()) return "";
 		       }        
 		   return prefix;
 	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		practice14 p = new practice14();
