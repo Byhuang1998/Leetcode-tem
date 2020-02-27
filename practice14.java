ade2c2a4d8e4e0efec541800e9ea26d79bb382c1
1160300526 <1181049074@qq.com>
2020-02-27 16:44:09 +0800

    longestCommonPrefix

--- practice14.java
+++ practice14.java
@@ -2,18 +2,14 @@
 public class practice14 {
 
 	public String longestCommonPrefix(String[] strs) {
-		String str = "";
-		if (strs == null || strs.length == 0) return "";
-		for(int j=0; j<strs[0].length(); j++) {
-			char c = strs[0].charAt(j);
-			for (int i=1; i<strs.length; i++) {
-				if (strs[i].length() == j || strs[i].charAt(j) != c)
-					return str;
-			}
-			str = str + strs[0].charAt(j);
-		}
-		// 当第一个字符串就是公共前缀时
-		return strs[0];
+		   if (strs.length == 0) return "";
+		   String prefix = strs[0];
+		   for (int i = 1; i < strs.length; i++)
+		       while (strs[i].indexOf(prefix) != 0) {
+		           prefix = prefix.substring(0, prefix.length() - 1);
+		           if (prefix.isEmpty()) return "";
+		       }        
+		   return prefix;
 	}
 
 	public static void main(String[] args) {
