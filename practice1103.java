1f1ade229c33b3a137ea9b44f5d2910f93cf0769
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-05 23:33:27 +0800

    bad mood

---
+++ practice1103.java
@@ -0,0 +1,19 @@
+
+public class practice1103 {
+
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int i = 0;
+        while (candies != 0) {
+            ans[i % num_people] += Math.min(candies, i + 1);
+            candies -= Math.min(candies, i + 1);
+            i += 1;
+        }
+        return ans;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
