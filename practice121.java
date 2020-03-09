b4d17ad86a2bb0c64f01a69bb1d9e9fb16f0c99d
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-09 09:26:37 +0800

    max profit

--- practice121.java
+++ practice121.java
@@ -6,15 +6,12 @@ public class practice121 {
 		int len = prices.length;
 		if (len < 2) return 0;
 		int profit = 0;
-		for (int i=0; i<len-1; i++) {
-			int temProfit = 0;
-			int tem = prices[i];
-			int temMax = prices[i+1];
-			for (int j=i+1; j<len; j++) {
-				if (prices[j]>temMax) {
-					temMax = prices[j];
-				}
-			}
+		int temMax = 0;
+		for (int i=len-2; i>=0; i--) {
+			int temProfit = 0; // 本次循环的最大利润
+			int tem = prices[i]; // 买入价
+			int temNum = prices[i+1];
+			temMax = temNum>temMax ? temNum : temMax;
 			temProfit = temMax>tem ? temMax - tem : 0;
 			profit = temProfit>profit ? temProfit : profit;
 		}
@@ -22,7 +19,7 @@ public class practice121 {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] prices = {10,2,3,4,5,6};
+		int[] prices = {10,5,3,4,5,6};
 		practice121 p = new practice121();
 		int profit = p.maxProfit(prices);
 		System.out.println(profit);
