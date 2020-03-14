80494f967bc7e90b93ecea23b889fbe9aca6513d
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-15 00:12:56 +0800

    longest increasing sequence

--- practice300.java
+++ practice300.java
@@ -2,29 +2,27 @@
 public class practice300 {
 
 	public int lengthOfLIS(int[] nums) {
-	        if (nums.length == 0) {
-	            return 0;
-	        }
-	        int[] dp = new int[nums.length];
-	        dp[0] = 1;
-	        int maxans = 1;
-	        for (int i = 1; i < dp.length; i++) {
-	            int maxval = 0;
-	            for (int j = 0; j < i; j++) {
-	                if (nums[i] > nums[j]) {
-	                    maxval = Math.max(maxval, dp[j]);
-	                }
-	            }
-	            dp[i] = maxval + 1;
-	            maxans = Math.max(maxans, dp[i]);
-	        }
-	        return maxans;
-	    
-	
+		int len = nums.length;
+		if (len == 0) return 0;
+		int max = 1;
+		int[] dp = new int[len];
+		dp[0] = 1;
+		for (int i=1; i<len; i++) {
+			int tem = 0;
+			dp[i] = 1;
+			for (int j=0; j<i; j++) {
+				if (nums[i]>nums[j]) {
+					tem = dp[j]+1;
+					if (tem>dp[i]) dp[i] = tem;
+				}
+			}
+			if (dp[i]>max) max = dp[i];
+		}
+		return max;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {-2,-1};
+		int[] nums = {0};
 		practice300 p = new practice300();
 		int ans = p.lengthOfLIS(nums);
 		System.out.println(ans);
