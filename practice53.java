c9af9bf6b1eb4166f943ea2c579afa9b1b5ae063
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-14 23:45:26 +0800

    find max substring

---
+++ practice53.java
@@ -0,0 +1,28 @@
+
+public class practice53 {
+
+	public int maxSubArray(int[] nums) {
+		int len = nums.length;
+		int res = nums[0];
+		int current = nums[0];
+		for (int i=1; i<len; i++) {
+			if (current<0) current = nums[i];
+			else current+=nums[i];
+			if (current>res) res = current;
+		}
+		
+		return res;
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		practice53 p = new practice53();
+		int[] nums = {-2,-3,-4};
+		int ans = p.maxSubArray(nums);
+		System.out.println(ans);
+	}
+
+}
