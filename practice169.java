49f5f08cf5248a5ca4df2cb3949afc30df35fc66
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-14 09:20:55 +0800

    find major element

--- practice169.java
+++ practice169.java
@@ -4,12 +4,10 @@ public class practice169 {
 	public int majorityElement(int[] nums) {
 		int len = nums.length;
 		ArrayList<Integer> list = new ArrayList<>();
-		int index = 0;
 		for (int i=0; i<len; i++) {
 			if (!list.contains(nums[i])) list.add(nums[i]);
 		}
 		int size = list.size();
-		int record = 0;
 		int ans = 0;
 		sign: for (int i=0; i<size; i++) {
 			int count = 0;
@@ -17,8 +15,6 @@ public class practice169 {
 				if (nums[j] == list.get(i)) count++;
 			}
 			if (count>len/2) {
-				record = count;
-				index = i;
 				ans = list.get(i);
 				break sign;
 			}
