ba65d9f11793d5b07eb137d86ca27711d5eab6e7
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-15 17:56:02 +0800

    find max area of island with Stack

--- practice695.java
+++ practice695.java
@@ -1,29 +1,38 @@
-
+import java.util.Stack;
 public class practice695 {
 
 	public int maxAreaOfIsland(int[][] grid) {
 		int row = grid.length;
 		int col = grid[0].length;
 		int max = 0;
+		int[] ii = {0,0,1,-1};
+		int[] jj = {1,-1,0,0};
+		
 		for (int i=0; i<row; i++) {
 			for (int j=0; j<col; j++) {
-				max = Math.max(max, dfs(grid, i, j));
+				int cur_num = 0;
+				Stack<Integer> stacki = new Stack<>();
+				Stack<Integer> stackj = new Stack<>();
+				if (grid[i][j]==0) continue;
+				stacki.push(i);
+				stackj.push(j);
+				while (!stacki.isEmpty()) {
+					int cur_i = stacki.pop();
+					int cur_j = stackj.pop();
+					if (cur_i<0 || cur_j<0 || cur_i==row || cur_j==col || grid[cur_i][cur_j]==0) continue;
+					cur_num++;
+					grid[cur_i][cur_j] = 0;
+					for (int k=0; k<4; k++) {
+						stacki.push(cur_i+ii[k]);
+						stackj.push(cur_j+jj[k]);
+					}
+				max = Math.max(max, cur_num);
+				}
 			}
 		}
+		
+		
 		return max;
-	}
-	
-	private int dfs(int[][] grid, int i, int j) {
-		int row = grid.length;
-		int col = grid[0].length;
-		if (i<0 || j<0 || i>=row || j>=col || grid[i][j]==0) return 0;
-		int num = 1;
-		grid[i][j] = 0;
-		num += dfs(grid, i+1, j);
-		num += dfs(grid, i-1, j);
-		num += dfs(grid, i, j+1);
-		num += dfs(grid, i, j-1);		
-		return num;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
