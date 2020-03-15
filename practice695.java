a9be65e827661dd9cd97333b58c6d903fff68dfb
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-15 18:20:38 +0800

    find max area of island with queue

--- practice695.java
+++ practice695.java
@@ -1,4 +1,5 @@
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Queue;
 public class practice695 {
 
 	public int maxAreaOfIsland(int[][] grid) {
@@ -7,31 +8,29 @@ public class practice695 {
 		int max = 0;
 		int[] ii = {0,0,1,-1};
 		int[] jj = {1,-1,0,0};
-		
+
 		for (int i=0; i<row; i++) {
 			for (int j=0; j<col; j++) {
-				int cur_num = 0;
-				Stack<Integer> stacki = new Stack<>();
-				Stack<Integer> stackj = new Stack<>();
 				if (grid[i][j]==0) continue;
-				stacki.push(i);
-				stackj.push(j);
-				while (!stacki.isEmpty()) {
-					int cur_i = stacki.pop();
-					int cur_j = stackj.pop();
-					if (cur_i<0 || cur_j<0 || cur_i==row || cur_j==col || grid[cur_i][cur_j]==0) continue;
+				Queue<Integer> q_i = new ArrayDeque<>();
+				Queue<Integer> q_j = new ArrayDeque<>();
+				int cur_num = 0;
+				q_i.offer(i);
+				q_j.offer(j);
+				while (!q_i.isEmpty()) {
+					int cur_i = q_i.poll();
+					int cur_j = q_j.poll();
+					if (cur_i<0 || cur_i==row || cur_j<0 || cur_j==col || grid[cur_i][cur_j]==0) continue;
 					cur_num++;
 					grid[cur_i][cur_j] = 0;
 					for (int k=0; k<4; k++) {
-						stacki.push(cur_i+ii[k]);
-						stackj.push(cur_j+jj[k]);
+						q_i.offer(cur_i+ii[k]);
+						q_j.offer(cur_j+jj[k]);
 					}
-				max = Math.max(max, cur_num);
 				}
+				max = Math.max(max, cur_num);
 			}
 		}
-		
-		
 		return max;
 	}
 	public static void main(String[] args) {
