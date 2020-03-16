33a814f697958d4866ddc22bc832898297cee13c
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-16 10:23:31 +0800

    rotting oranges

--- practice994.java
+++ practice994.java
@@ -1,56 +1,59 @@
-import java.util.ArrayList;
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.HashMap;
 public class practice994 {
 	
 	public int orangesRotting(int[][] grid) {
-		// 获取行列长度
-		int col = grid.length;
-		int row = grid[0].length;
-		
-		// 需要花费的时间
+		int row = grid.length;
+		if (row==0) return 0;
+		int col = grid[0].length;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int[] ii = {1,-1,0,0};
+		int[] jj = {0,0,1,-1};
 		int time = 0;
-		// 分别用于存储空格子的坐标	
-		ArrayList<Integer> emptyCol = new ArrayList<>();
-		ArrayList<Integer> emptyRow = new ArrayList<>();
-		int emptyCount = 0;		
-		
-		// 分别用于存储刚开始的腐烂的橘子的坐标	
-		ArrayList<Integer> freshCol = new ArrayList<>();
-		ArrayList<Integer> freshRow = new ArrayList<>();
-		int freshCount = 0;		
-		
-		// 分别用于存储刚开始的腐烂的橘子的坐标	
-		ArrayList<Integer> rottenCol = new ArrayList<>();
-		ArrayList<Integer> rootenRow = new ArrayList<>();
-		int rottenCount = 0;
-		
-		// 找到初始时，空格子、新鲜、腐烂的位置
-		for (int i=0; i<col; i++) {
-			
-			for (int j=0; j<row; j++) {
-				if (grid[i][j] == 0) {
-					emptyCol.set(emptyCount, i);
-					emptyRow.set(emptyCount, j);
-					emptyCount++;
-					
-				} else	if (grid[i][j] == 1) {
-					freshCol.set(freshCount, i);
-					freshRow.set(freshCount, j);
-					freshCount++;
-				} else {
-					rottenCol.set(rottenCount, i);
-					rootenRow.set(rottenCount, j);
-					rottenCount++;
+		Queue<Integer> q_i = new ArrayDeque<>(); // 存储横坐标
+		Queue<Integer> q_j = new ArrayDeque<>(); // 存储纵坐标
+		// 将初始腐烂的橘子放入HashMap中，其键表示位置，值表示发生腐烂的时间。
+		for (int i=0; i<row; i++) {
+			for (int j=0; j<col; j++) {
+				if (grid[i][j]==2) {
+					int code = i * col + j;
+					map.put(code, 0);
+					q_i.offer(i);
+					q_j.offer(j);
 				}
 			}
-		}		
+		}
+		while (!q_i.isEmpty()) {
+			int cur_i = q_i.poll();
+			int cur_j = q_j.poll();
+			int code = cur_i * col + cur_j;
+			for (int k=0; k<4; k++) {
+				int tem_i = cur_i + ii[k];
+				int tem_j = cur_j + jj[k];
+				if (tem_i>=0 && tem_i<row && tem_j>=0 && tem_j<col && grid[tem_i][tem_j]==1) {
+					grid[tem_i][tem_j] = 2;
+					int ncode = tem_i * col + tem_j;
+					map.put(ncode, map.get(code)+1);
+					q_i.offer(tem_i);
+					q_j.offer(tem_j);
+					time = map.get(ncode);
+				}
+			}
+		}
+		for (int i=0; i<row; i++) {
+			for (int j=0; j<col; j++) {
+				if (grid[i][j] == 1) return -1; 
+			}
+		}
 		return time;
 	}
-
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		int[][] grid = {{1},{2}};
+		practice994 p = new practice994();
+		int time = p.orangesRotting(grid);
+		System.out.println(time);
 	}
-
 }
