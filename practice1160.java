4c948eb9c28125eb9214fce34e8adb76749cec47
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-18 00:08:37 +0800

    spell the words

--- practice1160.java
+++ practice1160.java
@@ -1,31 +1,42 @@
-import java.util.Stack;
+import java.util.HashMap;
 public class practice1160 {
 
 	public int countCharacters(String[] words, String chars) {
-		int len = words.length;
-		if (len==0) return 0;
+		int num = words.length;
+		int len = chars.length();
 		int res = 0;
-		int num = chars.length();
+		HashMap<Character, Integer> char_map = new HashMap<>();
 		for (int i=0; i<len; ++i) {
-			Stack<Character> s = new Stack<>();
-			for (int j=0; j<num; ++j) s.push(chars.charAt(j));//将所有字母都压入栈中
-			String cur = words[i];
-			int cur_len = cur.length();
-			int count= 0;
-			for (int k=0; k<num; ++k) {				
-				String tem = s.pop()+"";
-				if (cur.contains(tem)) {
-					cur = cur.replaceFirst(tem, "");
-					count++;
-					
-				}
-				if (s.isEmpty() && !cur.isEmpty()) {
+			char tem = chars.charAt(i);
+			if (!char_map.containsKey(tem))
+				char_map.put(tem, 1);
+			else 
+				char_map.put(tem, char_map.get(tem)+1);
+		}
+		for (int i=0; i<num; i++) {
+			int count = 0;
+			String word = words[i];
+			int cur_len = word.length();
+			HashMap<Character, Integer> word_map = new HashMap<>();
+			for (int j=0; j<cur_len; j++) {
+				char tem = word.charAt(j);
+				if (!word_map.containsKey(tem))
+					word_map.put(tem, 1);
+				else 
+					word_map.put(tem, word_map.get(tem)+1);			
+			}
+			for (int j=0; j<cur_len; ++j) {
+				char cur_char = word.charAt(j);
+				count = cur_len;
+				if (!char_map.containsKey(cur_char) || word_map.get(cur_char) > char_map.get(cur_char)) {
 					count = 0;
 					break;
 				}
 			}
 			res += count;
+				
 		}
+		
 		return res;
 	}
 	public static void main(String[] args) {
