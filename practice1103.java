054a1a4eb57b768774f15b56e430fd86333ce6ab
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-18 19:37:31 +0800

    distribute candy

--- practice1103.java
+++ practice1103.java
@@ -1,19 +1,27 @@
+import java.util.Arrays;
 
 public class practice1103 {
 
     public int[] distributeCandies(int candies, int num_people) {
-        int[] ans = new int[num_people];
-        int i = 0;
-        while (candies != 0) {
-            ans[i % num_people] += Math.min(candies, i + 1);
-            candies -= Math.min(candies, i + 1);
-            i += 1;
-        }
-        return ans;
+    	int[] res = new int[num_people];
+    	int i=1;
+    	while (candies!=0) {
+    		int next = Math.min(candies, i);
+    		res[(i-1)%num_people] += next;
+    		i++;
+    		candies -= next;
+    	}
+    	
+    	return res;
     }
+    
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		practice1103 p = new practice1103();
+		int candies = 10;
+		int num_people = 3;
+		System.out.println(Arrays.toString(p.distributeCandies(candies, num_people)));
+		
 	}
 }
