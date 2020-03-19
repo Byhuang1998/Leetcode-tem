30f141c63f4670dfd70adf90c9cd40b2866c118e
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-19 12:31:13 +0800

    length of last word

--- practice58.java
+++ practice58.java
@@ -2,14 +2,20 @@
 public class practice58 {
 
 	public int lengthOfLastWord(String s) {
-		String[] ss = s.split(" ");
-		if (ss.length==0) return 0;
-		System.out.println(ss[ss.length-1].toString());
-		return ss[ss.length-1].length();
+		if (s.length()==0) return 0;
+		int i = s.length()-1;
+		while (i>=0 && s.charAt(i) == ' ') {
+			i--;
+		}
+		int j = i;
+		while (j>=0 && s.charAt(j) != ' ') {
+			--j;
+		}
+		return i-j;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "Hello World";
+		String s = " ";
 		System.out.println(new practice58().lengthOfLastWord(s));
 
 	}
