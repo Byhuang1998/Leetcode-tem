2e6a6c848bc9ae473e642fc4ca8dacfc84e143cd
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-28 23:07:14 +0800

    minimum encoding of string[]

--- test.java
+++ test.java
@@ -1,27 +1,43 @@
 
-public class test {
-
-	public class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int x) { val = x;}
+import java.util.HashMap;
+class TireNode {
+	int count = 0;
+	TireNode[] children = new TireNode[26];
+	public TireNode get(char c) {
+		if (children[c-'a'] == null) {
+			children[c-'a'] = new TireNode();
+			count ++;
+		}
+		return children[c-'a'];
 	}
-	public int maxDepth(TreeNode root) {
-		if (root == null) return 0;
-		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
+public class test {
+	public int minimumLengthEncoding(String[] words) {
+		int ans = 0;
+		HashMap<TireNode, Integer> map = new HashMap<>();
+		int num = words.length;
+		TireNode root = new TireNode();
+		for (int i=0; i<num; ++i) {
+			TireNode cur_root = root;
+			String word = words[i];
+			for (int j=word.length()-1; j>=0; --j) {
+				cur_root = cur_root.get(word.charAt(j));
+			}
+			map.put(cur_root, i);
+		}
+		for (TireNode tn : map.keySet()) {
+			if (tn.count == 0) ans += words[map.get(tn)].length() + 1;
+		}
+		return ans;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		test p = new test();
-		// 根
-		TreeNode root = p.new TreeNode(1);
-		// 第二层
-		TreeNode l = root.left = p.new TreeNode(3);	TreeNode r = root.right = p.new TreeNode(3);
-		// 第三层
-		l.left = p.new TreeNode(4);	l.right = p.new TreeNode(3);
-		r.left = p.new TreeNode(3);	r.right = p.new TreeNode(4);
-		System.out.println(p.maxDepth(root));
+
+		String[] ss = {"time", "me", "bell"};
+		int len = p.minimumLengthEncoding(ss);
+		System.out.println(len);
 
 	}
 
