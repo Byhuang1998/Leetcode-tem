01cf745d524653e9fa4f7f29cd5e4aff54b8b22c
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-01 23:10:02 +0800

    sort

--- practice912.java
+++ practice912.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 public class practice912 {
 
+	// 冒泡排序
 	public int[] bubble(int[] nums) {
 		int len = nums.length;
-		for (int i=0; i<len; ++i) {
+		for (int i=0; i<len-1; ++i) {
 			for (int j=0; j<len-1-i; ++j) {
 				if (nums[j]>nums[j+1]) {
 					// 交换两个数
@@ -17,11 +18,31 @@ public class practice912 {
 		
 		return nums;
 	}
+	
+	// 选择算法
+	public int[] select(int[] nums) {
+		int len = nums.length;
+		for (int i=0; i<len; i++) {
+			int flag = i;
+			int cur = nums[i];
+			for (int j=i; j<len; ++j) {
+				if (nums[j]<cur) {
+					flag = j;
+					cur = nums[j];
+				}
+			}
+			int tem = nums[flag];
+			nums[flag] = nums[i];
+			nums[i] = tem;
+		}
+		
+		return nums;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {6,5,4,3,2,1};
+		int[] nums = {6,5,4,3,2,1,2,5,9,8,7};
 		practice912 p = new practice912();
-		int[] ans = p.bubble(nums);
+		int[] ans = p.select(nums);
 		System.out.println(Arrays.toString(ans));
 
 	}
