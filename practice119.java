2d571ac0cab0c5c517cecc09df3a2ce97dcb4a5d
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-04 00:30:42 +0800

    打印杨辉三角的某一行

--- practice119.java
+++ practice119.java
@@ -3,29 +3,43 @@ import java.util.List;
 public class practice119 {
 
 	public List<Integer> getRow(int rowIndex) {
-		List<Integer> preRow = new ArrayList<>();
-		List<Integer> curRow = new ArrayList<>();
- 		preRow.add(1);
- 		if (rowIndex == 0) return preRow;
-		curRow.add(1); curRow.add(1);
-		if (rowIndex == 1) return curRow;
- 		for (int i=2; i<=rowIndex; ++i) {
- 			preRow = curRow;
-			List<Integer> temRow = new ArrayList<>();
-			temRow.add(1);
-			for (int k=1; k<i; ++k) {
-				int tem = preRow.get(k-1) + preRow.get(k);
-				temRow.add(tem);
+		List<Integer> list = new ArrayList<>();
+ 		list.add(1);
+ 		if (rowIndex == 0) return list;
+		list.add(1);
+		if (rowIndex == 1) return list;
+		for (int i=2; i<=rowIndex; ++i) {
+			// 是偶数列，意味着这一行有奇数个数
+			int size = list.size();
+			if (i % 2 == 0) {
+				for (int j=1; j<i/2; ++j) {
+					list.set(j, list.get(i-j) + list.get(i-j-1)); 
+					if (j+1 == rowIndex / 2)	list.set(j, 2 * list.get(j));
+				}
+				for (int j=i/2+1; j<=i; ++j) {
+					size = list.size();
+					if (j >= size) list.add(0);
+					list.set(j, list.get(i-j)); 
+				}
 			}
-			temRow.add(1);
-			curRow = temRow;
+			// 奇数列，意味着这一行有偶数个数
+			else {
+				for (int j=1; j<=i/2; ++j) {
+					list.set(j, list.get(i-j) + list.get(i-j-1)); 
+				}
+				for (int j=i/2+1; j<=i; ++j) {
+					size = list.size();
+					if (j >= size) list.add(0);
+					list.set(j, list.get(i-j)); 
+				}
+			}
 		}
-		return curRow;
+		return list;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		practice119 p = new practice119();
-		List<Integer> ans = p.getRow(1);
+		List<Integer> ans = p.getRow(2);
 		System.out.println(ans);
 	}
 
