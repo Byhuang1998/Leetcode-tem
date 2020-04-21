0377446169ba0b6e08c5bda5ca570d0ec0365229
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-21 23:51:45 +0800

    完美子数组

--- practice1248.java
+++ practice1248.java
@@ -1,65 +1,44 @@
-import java.util.ArrayList;
-public class practice1248 {
-	    public boolean isOdd(int n) {
-	        return ! (n % 2 == 0);
-	    }
-	    public int getFirst(int[] nums, int len, int k) {
-	        int i = 0;
-	        for (; i<len; i++) {
-	            if (isOdd(nums[i])) {
-	                 return i;
-	            }
-	        }
-	        return -1;
-	    }
 
-	    public int getPos(int[] nums, int len, int k) {
-	        int sum = 0;
-	        int i = 0;
-	        for (; i<len; i++) {
-	            if (isOdd(nums[i])) {
-	                sum ++;
-	                if (sum == k) return i;
-	            }
-	        }
-	        return -1;
-	    }
-	    public int numberOfSubarrays(int[] nums, int k) {
-	        int len = nums.length;
-	        ArrayList<Integer> list = new ArrayList<>();
-	        for (int i=0; i<len; i++) {
-	            if (isOdd(nums[i])) list.add(i);
-	        }
-	        int n = list.size();
-	        int p1 = getFirst(nums, len, k);
-	        int p2 = getPos(nums, len, k);
-	        if (p2 == -1) return 0;
-	        if (p2 == list.get(n-1)) return (p1+1) * (len-p2);
-	        int count = p1 + 1;
-	        while (p2 < len-1) {
-	        	p2 ++;
-	        	// 如果是奇数
-	        	if (isOdd(nums[p2])) {
-	        		while (true) {
-	        			p1 ++;
-	        			if (isOdd(nums[p1])) {
-	        				count++;
-	        				break;
-	        			}
-	        			else {
-	        				count++;
-	        			}
-	        		}
-	        	}
-	        	// 如果是偶数
-	        	else {
-	        		count ++;
-	        		p2 ++;
-	        	}
-	 
-	        }
-	        return count + len -1 -p2;
-	    }
+public class practice1248 {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int left = 0, right = 0, oddCnt = 0, res = 0;
+        while (right < nums.length) {
+            // 右指针先走，每遇到一个奇数则 oddCnt++。
+            if ((nums[right++] & 1) == 1) {
+                oddCnt++;
+            }
+
+            //  若当前滑动窗口 [left, right) 中有 k 个奇数了，进入此分支统计当前滑动窗口中的优美子数组个数。
+            if (oddCnt == k) {
+                // 先将滑动窗口的右边界向右拓展，直到遇到下一个奇数（或出界）
+                // rightEvenCnt 即为第 k 个奇数右边的偶数的个数
+                int tmp = right;
+                while (right < nums.length && (nums[right] & 1) == 0) {
+                    right++;
+                }
+                int rightEvenCnt = right - tmp;
+                // leftEvenCnt 即为第 1 个奇数左边的偶数的个数
+                int leftEvenCnt = 0;
+                while ((nums[left] & 1) == 0) {
+                    leftEvenCnt++;
+                    left++;
+                }
+                // 第 1 个奇数左边的 leftEvenCnt 个偶数都可以作为优美子数组的起点
+                // (因为第1个奇数左边可以1个偶数都不取，所以起点的选择有 leftEvenCnt + 1 种）
+                // 第 k 个奇数右边的 rightEvenCnt 个偶数都可以作为优美子数组的终点
+                // (因为第k个奇数右边可以1个偶数都不取，所以终点的选择有 rightEvenCnt + 1 种）
+                // 所以该滑动窗口中，优美子数组左右起点的选择组合数为 (leftEvenCnt + 1) * (rightEvenCnt + 1)
+                res += (leftEvenCnt + 1) * (rightEvenCnt + 1);
+
+                // 此时 left 指向的是第 1 个奇数，因为该区间已经统计完了，因此 left 右移一位，oddCnt--
+                left++;
+                oddCnt--;
+            }
+
+        }
+
+        return res;
+    }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
