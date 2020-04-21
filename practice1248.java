65b43794a7283774bbbc77a08b8ac120889e5293
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-21 23:40:32 +0800

    完美子数组

---
+++ practice1248.java
@@ -0,0 +1,65 @@
+
+public class practice1248 {
+	    public boolean isOdd(int n) {
+	        return ! (n % 2 == 0);
+	    }
+	    public int getFirst(int[] nums, int len, int k) {
+	        int i = 0;
+	        for (; i<len; i++) {
+	            if (isOdd(nums[i])) {
+	                 return i;
+	            }
+	        }
+	        return -1;
+	    }
+
+	    public int getPos(int[] nums, int len, int k) {
+	        int sum = 0;
+	        int i = 0;
+	        for (; i<len; i++) {
+	            if (isOdd(nums[i])) {
+	                sum ++;
+	                if (sum == k) return i;
+	            }
+	        }
+	        return -1;
+	    }
+	    public int numberOfSubarrays(int[] nums, int k) {
+	        int len = nums.length;
+	        int p1 = getFirst(nums, len, k);
+	        int p2 = getPos(nums, len, k);
+	        if (p2 == -1) return 0;
+	        int count = p1 + 1;
+	        while (p2 < len-1) {
+	        	p2 ++;
+	        	// 如果是奇数
+	        	if (isOdd(nums[p2])) {
+	        		while (true) {
+	        			p1 ++;
+	        			if (isOdd(nums[p1])) {
+	        				count++;
+	        				break;
+	        			}
+	        			else {
+	        				count++;
+	        			}
+	        		}
+	        	}
+	        	// 如果是偶数
+	        	else {
+	        		count ++;
+	        	}
+	 
+	        }
+	        return count + len -1 -p2;
+	    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		practice1248 p = new practice1248();
+		int[] nums = {2,2,2,1,2,2,1,2,2,2};
+		//int[] nums = {45627,50891,94884,11286,35337,46414,62029,20247,72789,89158,54203,79628,25920,16832,47469,80909};
+		System.out.println(p.numberOfSubarrays(nums, 2));
+	}
+
+}
