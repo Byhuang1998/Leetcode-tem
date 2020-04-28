25b914256450580eb5057c0d0c0516ea884547b6
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-28 16:50:51 +0800

    find single number

--- practice137.java
+++ practice137.java
@@ -1,9 +1,26 @@
-
+import java.util.HashSet;
+import java.util.Set;
 public class practice137 {
+	
+
+    public int singleNumber(int[] nums) {
+		Set<Long> set = new HashSet<>();
+		long s = 0;
+		for (long num : nums) {
+			set.add(num);
+			s += num;
+		}
+		long sum = 0;
+		for (long num : set) {
+			sum += num;
+		}
+		
+		return (int)((sum * 3 - s) / 2);
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
