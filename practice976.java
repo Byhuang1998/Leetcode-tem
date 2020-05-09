4286c8acfe6044e0f3c0ac462f81a70e6d4435d0
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-09 23:20:29 +0800

    largest perimeter

--- practice976.java
+++ practice976.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
 
 public class practice976 {
 
+	public int largestPerimeter(int[] A) {
+		Arrays.sort(A);
+		for (int l = A.length - 3; l >= 0; l--) {
+			if (A[l+2] < A[l+1] + A[l]) {
+				return A[l+2] + A[l+1] + A[l];
+			}
+		}
+		
+		return 0;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
