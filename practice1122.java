eabc641dfc4b44adde46ea94395a1b2fcbb86148
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-09 18:28:18 +0800

    relative sort array

--- practice1122.java
+++ practice1122.java
@@ -5,39 +5,27 @@ public class practice1122 {
 	public int[] relativeSortArray(int[] arr1, int[] arr2) {
 		int m = arr1.length;
 		int n = arr2.length;
+		int[] count = new int[1001];
+		// ¼ÆÊý
+		for (int i=0; i<m; i++) {
+			count[arr1[i]] ++;
+		}
 		int index = 0;
 		for (int i=0; i<n; i++) {
 			int cur = arr2[i];
-			for (int j=index; j<m; j++) {
-				if (arr1[j] == cur) {
-					swap(arr1, index, j);
-					index ++;
+			for (int j=0; j<count[cur]; j++) {
+				arr1[index++] = cur;
+			}
+			count[cur] = 0;
+		}
+		for (int i=0; i<1001; i++) {
+			if (count[i] != 0) {
+				for (int j=0; j<count[i]; j++) {
+					arr1[index++] = i;
 				}
 			}
 		}
-		sort(arr1, m, index);
-		return arr1;
-	}
-	
-	public void swap(int[] arr1, int i, int j) {
-		int cur = arr1[i];
-		arr1[i] = arr1[j];
-		arr1[j] = cur;
-	}
-	
-	public void sort(int[] arr1, int m, int index) {
-//		int n = m - index;
-		for (int i=index; i<m; i++) {
-			int k = i;
-			int cur = arr1[i];
-			for (int j=i; j<m; j++) {
-				if (arr1[j] < cur) {
-					cur = arr1[j];
-					k = j;
-				}
-			}
-			swap(arr1, i, k);
-		}
+		return arr1;	
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
