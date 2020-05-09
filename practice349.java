bdab4f2694a81d2eefb37bf4953bd985a95e2406
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-09 23:02:06 +0800

    intersect of two arrays

--- practice349.java
+++ practice349.java
@@ -1,49 +1,35 @@
-class Solution {
-    public void sort(int[] nums, int len) {
-        for (int i=0; i<len-1; i++) {
-            int index = i;
-            int tem = nums[i];
-            for (int j=i+1; j<len; j++) {
-                if (nums[j] < tem) {
-                    index = j;
-                    tem = nums[j];
-                }
-            }
-            if (index > i) {
-                int num = nums[i];
-                nums[i] = nums[index];
-                nums[index] = num;
-            }
-        }
-    }
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+
+class practice349 {
+	
     public int[] intersection(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (len1 == 0 || len2 == 0) return new int[0];
-        sort(nums1, len1);
-        sort(nums2, len2);
-        List<Integer> list = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        int count = 0;
-        while (i < len1 && j < len2) {
-            if (nums1[i] == nums2[j]) {
-                if (count == 0 || nums1[i] != list.get(count-1)) {
-                    list.add(nums1[i]);
-                    count ++;
-                }
-                i ++;
-                j ++;
-            } else if (nums1[i] < nums2[j]) {
-                i ++;
-            } else {
-                j ++;
-            }
-        }
-        int[] ans = new int[count];
-        for (int k=0; k<count; k++) {
-            ans[k] = list.get(k);
-        }
-        return ans;
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+			s1.add(nums1[i]);
+		}
+        for (int i = 0; i < nums2.length; i++) {
+			s2.add(nums2[i]);
+		}
+        int[] res = new int[nums1.length];
+        int index = 0;
+        for (int i : s2) {
+			if (s1.contains(i)) {
+				res[index++] = i;
+			}
+		}
+        return Arrays.copyOf(res, index);
     }
+    
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums1 = {1,2,2,1};
+		int[] nums2 = {1,2};
+		practice349 p = new practice349();
+		System.out.println(Arrays.toString(p.intersection(nums1, nums2)));
+    	
+    }
+    
 }
