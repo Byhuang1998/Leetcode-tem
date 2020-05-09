eb8ccc2e6e3016ccba3ebcabc03fb5bd1819ad07
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-09 19:37:28 +0800

    is anagram

--- practice242.java
+++ practice242.java
@@ -5,38 +5,20 @@ public class practice242 {
 		int l1 = s.length();
 		int l2 = t.length();
 		if (l1 != l2) return false;
-		char[] sc = s.toCharArray();
-		char[] tc = t.toCharArray();
-		sortString(sc, l1);
-		sortString(tc, l2);
+		int[] ss = new int[26];
+		int[] tt = new int[26];
 		for (int i=0; i<l1; i++) {
-			if (sc[i] != tc[i]) {
+			ss[s.charAt(i) - 'a'] ++;
+			tt[t.charAt(i) - 'a'] ++;
+		}
+		for (int i=0; i<26; i++) {
+			if (ss[i] != tt[i]) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	public void sortString(char[] sc, int l1) {
-		for (int i=0; i<l1; i++) {
-			char cur = sc[i];
-			int index = i;
-			for (int j=i; j<l1; j++) {
-				if (sc[j] < cur) {
-					cur = sc[j];
-					index = j;
-				}
-			}
-			swap(sc, i, index);
-		}
-	}
 
-	private void swap(char[] sc, int i, int index) {
-		// TODO Auto-generated method stub
-		char tem = sc[i];
-		sc[i] = sc[index];
-		sc[index] = tem;
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
