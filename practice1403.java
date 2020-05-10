64cda4675fac2e94b332f14ebe28f5eb3274a920
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-10 22:58:21 +0800

    sort

--- practice1403.java
+++ practice1403.java
@@ -1,19 +1,22 @@
 class Solution {
     public List<Integer> minSubsequence(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> list = new ArrayList<>();
+        int[] bucket = new int[101];
         int sum = 0;
         for (int n : nums) {
             sum += n;
+            bucket[n] ++;
         }
-        int index = nums.length - 1;
-        int temSum = nums[index--];
-        while (temSum <= sum - temSum) {
-            temSum += nums[index--];
-        }
-        for (int i = nums.length - 1; i > index; --i) {
-            list.add(nums[i]);
-        }
+        int curSum = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i=100; i>=0 && curSum <= sum - curSum; i--) {
+            if (bucket[i] > 0) {
+                while (bucket[i] > 0 && curSum <= sum - curSum) {
+                    list.add(i);
+                    curSum += i;
+                    bucket[i] --;
+                }
+            }
+        } 
         return list;
     }
 }
