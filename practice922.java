ae99d36a8911e722436ed796ff5fde8e9c903007
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-10 23:14:43 +0800

    sort

--- practice922.java
+++ practice922.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] sortArrayByParityII(int[] A) {
-        int[] res = new int[A.length];
-        int i = 0;
-        int j = 1;
-        for (int n : A) {
-            if (n % 2 == 0) {
-                res[i] = n;
-                i += 2;
-            } else {
-                res[j] = n;
-                j += 2;
+        int i = 0, j=1;
+        for (; i < A.length; i += 2) {
+            if (A[i] %2 == 1) {
+                while (A[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(A, i, j);
             }
         }
-        return res;
+        return A;
+    }
+
+    private void swap(int[] A, int i, int j) {
+        int tem = A[i];
+        A[i] = A[j];
+        A[j] = tem;
     }
 }
