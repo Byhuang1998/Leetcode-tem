b7bc4b0c6597e53472af9ac1028a3e19aeb4c854
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-12 13:59:32 +0800

    栈的练习

--- practice1021.java
+++ practice1021.java
@@ -1,23 +1,17 @@
 class Solution {
     public String removeOuterParentheses(String S) {
-        Stack<Character> s = new Stack<>();
-        char[] sc = S.toCharArray();
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < S.length(); i++) {
-            if (sc[i] == '(') {
-                s.push(sc[i]);
-            } else {
-                s.pop();
-                if (s.isEmpty()) {
-                    list.add(i);
-                }
-            }
-        }
+        int level = 0;
         StringBuilder sb = new StringBuilder();
-        int start = 0;
-        for (int i : list) {
-            sb.append(S.substring(start + 1, i));
-            start = i + 1;
+        for (char c : S.toCharArray()) {
+            if (c == '(') {
+                level++;
+            }
+            if (level > 1) {
+                sb.append(c);
+            }
+            if (c == ')') {
+                level--;
+            }
         }
         return sb.toString();
     }
