1fae4d221b856a5939fe44b724aa0b1e68a1537b
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-13 15:15:43 +0800

    相邻不超过k的相同的数

--- practice219.java
+++ practice219.java
@@ -1,22 +1,13 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // 考虑k比数组大小大或相等的情形，不能用下面的循环判断
-        if (k >= nums.length) {
-            Arrays.sort(nums);
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i + 1]) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            int cur = nums[i];
-            // 要考虑可能越界的情况，所以循环退出条件要仔细
-            for (int j = 1; j <= k && i + j < nums.length; j++) {
-                if (nums[i + j] == cur) {
-                    return true;
-                }
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
             }
         }
         return false;
