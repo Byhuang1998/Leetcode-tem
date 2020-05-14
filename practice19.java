ce08985fca0506f88c39ec7713c306b9037bac3f
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-14 13:39:47 +0800

    删除倒数链表的倒数第k个节点

--- practice19.java
+++ practice19.java
@@ -8,28 +8,17 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (n == 1) {
-            ListNode sen = new ListNode(0);
-            sen.next = head;
-            ListNode pre = sen, cur = head;
-
-            while (cur.next != null) {
-                pre = pre.next;
-                cur = cur.next;
-            }
-            pre.next = null;
-            return sen.next;
-        }
-        ListNode fast = head, slow = head;
-        for (int i = 0; i < n; i++) {
+        ListNode sen = new ListNode(0);
+        sen.next = head;
+        ListNode fast = sen, slow = sen;
+        for (int i = 0; i <= n; i++) {
             fast = fast.next;
         }
         while (fast != null) {
             fast = fast.next;
             slow = slow.next;
         }
-        slow.val = slow.next.val;
         slow.next = slow.next.next;
-        return head;
+        return sen.next;
     }
 }
