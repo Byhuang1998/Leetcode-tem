5980dfaa43754cdba9bd729b3ece7ec0c9232ac1
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-14 22:53:27 +0800

    链表操作

--- practice86.java
+++ practice86.java
@@ -9,35 +9,21 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
         if (head == null || head.next == null) return head;
-        ListNode l = head;
-        int len = 0, flag = Integer.MAX_VALUE / 2;
-        boolean found = false;
-        List<Integer> list = new ArrayList<>();
+        ListNode sentry1 = new ListNode(0);
+        ListNode sentry2 = new ListNode(0);
+        ListNode l = head, first = sentry1, second = sentry2;
         while (l != null) {
-            list.add(l.val);
-            if (l.val >= x && found == false) {
-                flag = len;
-                found = true;
+            if (l.val < x) {
+                first.next = new ListNode(l.val);
+                first = first.next;
+            } else {
+                second.next = new ListNode(l.val);
+                second = second.next;
             }
             l = l.next;
-            len++;
         }
-        for (int i = flag + 1; i < len; i++) {
-            if (list.get(i) < x) {
-                int tem = list.get(i);
-                list.remove(i);
-                list.add(flag, tem);
-                flag++;
-            }
-        }
-        ListNode sentry = new ListNode(0);
-        ListNode pre = sentry;
-        for (int n : list) {
-            ListNode ln = new ListNode(n);
-            pre.next = ln;
-            pre = ln;
-        }
-        pre.next = null;
-        return sentry.next;
+        first.next = sentry2.next;
+        second.next = null;
+        return sentry1.next;
     }
 }
