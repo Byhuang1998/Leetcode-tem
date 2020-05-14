3d51e3bcc45f2ebcc1fd019a9f05bbcd5056b3ea
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-14 21:27:54 +0800

    链表旋转

---
+++ practice61.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        int len = 1;
+        while (cur.next != null) {
+            len++;
+            cur = cur.next;
+        }
+        cur.next = head;
+        cur = head;
+        int r = len - k - 1;
+        while (r < 0) {
+            r += len;
+        }
+        for (int i = 0; i < r; i++) {
+            cur = cur.next;
+        }
+        ListNode res = cur.next;
+        cur.next = null;
+        return res;
+    }
+}
