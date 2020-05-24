f38ad803f9a184046244783f84779339477e2d74
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-24 23:48:38 +0800

    所有左叶子之和

---
+++ practice404.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return helper(root, false);
+    }
+
+    private int helper(TreeNode root, boolean isLeft) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null && isLeft) {
+            return root.val;
+        } 
+        return helper(root.left, true) + helper(root.right, false);
+    }
+}
