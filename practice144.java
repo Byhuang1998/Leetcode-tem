129fa3fd60a2779b160346e7c21d4e476e9314d8
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-28 01:48:33 +0800

    二叉树的前序遍历

--- practice144.java
+++ practice144.java
@@ -8,12 +8,20 @@
  * }
  */
 class Solution {
-    List<Integer> res = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        if (root != null) {
-            res.add(root.val);
-            preorderTraversal(root.left);
-            preorderTraversal(root.right);
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        while (! s.isEmpty()) {
+            TreeNode cur = s.pop();
+            res.add(cur.val);
+            if (cur.right != null) {
+                s.push(cur.right);
+            }
+            if (cur.left != null) {
+                s.push(cur.left);
+            }
         }
         return res;
     }
