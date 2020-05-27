f7cc8e0a93733ea4cf2eb2a4d8f98f22d50bd5a1
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-28 03:13:59 +0800

    二叉树的后序遍历

---
+++ practice145.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root != null) {
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            res.add(root.val);
+        }
+        return res;
+    }
+}
