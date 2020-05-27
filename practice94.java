e3f03d6af90b112df0465b79b048a44b61e9bfb8
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-27 23:37:42 +0800

    二叉树的中序遍历

---
+++ practice94.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> res = new LinkedList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            res.add(root.val);
+            inorderTraversal(root.right);
+        }
+    return res;
+    }
+}
