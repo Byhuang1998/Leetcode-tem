736ac6281099f607780cecc79ad5025cbd145e64
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-28 15:30:06 +0800

    二叉树的后序遍历

--- practice145.java
+++ practice145.java
@@ -8,13 +8,22 @@
  * }
  */
 class Solution {
-    List<Integer> res = new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        if (root != null) {
-            postorderTraversal(root.left);
-            postorderTraversal(root.right);
-            res.add(root.val);
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        while (! s.isEmpty()) {
+            TreeNode cur = s.pop();
+            res.add(cur.val);
+            if (cur.left != null) {
+                s.push(cur.left);
+            }
+            if (cur.right != null) {
+                s.push(cur.right);
+            }
         }
+        Collections.reverse(res);
         return res;
     }
 }
