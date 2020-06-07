7da2255c3f9c59612155777c05372db9a9c9d0ce
Byhuang1998 <Byhuang1998@gmail.com>
2020-06-08 01:23:06 +0800

    二叉搜索树查值

---
+++ practice700.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null && root.val != val) {
+            if (root.val > val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return root;
+    }
+}
