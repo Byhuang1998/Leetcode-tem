840f088d70c1ab825cce6816b564d8b807bd4f47
Byhuang1998 <Byhuang1998@gmail.com>
2020-06-15 21:47:46 +0800

    ping number

---
+++ practice933.java
@@ -0,0 +1,20 @@
+class RecentCounter {
+    Queue<Integer> q; 
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.offer(t);
+        while (!q.isEmpty() && q.peek() < t - 3000) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
