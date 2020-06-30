2e987b858e2003dafb5481220c667f07ccb6ecf1
Byhuang1998 <Byhuang1998@gmail.com>
2020-06-30 22:59:19 +0800

    计算二叉树的深度

--- practice104.java
+++ practice104.java
@@ -1,48 +1,31 @@
-import java.util.Stack;
-import java.util.HashMap;
-public class practice104 {
-
-	public class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int x) { val = x;}
-	}
-	public int maxDepth(TreeNode root) {
-		if (root == null) return 0;
-		int max = 0;
-		HashMap<TreeNode, Integer> map = new HashMap<>();
-		Stack<TreeNode> s = new Stack<>();
-		TreeNode cur = root;
-		s.push(cur);
-		map.put(cur, 1);
-		while (! s.isEmpty()) {
-			cur = s.pop();
-			int depth = map.get(cur);
-			max = Math.max(max, depth);
-			if (cur.right != null) {
-				s.push(cur.right);
-				map.put(cur.right, depth+1);
-			}
-			if (cur.left != null) {
-				s.push(cur.left);
-				map.put(cur.left, depth+1);
-			}
-		}
-		return max;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		practice104 p = new practice104();
-		// 根
-		TreeNode root = p.new TreeNode(1);
-		// 第二层
-		TreeNode l = root.left = p.new TreeNode(3);	TreeNode r = root.right = p.new TreeNode(3);
-		// 第三层
-		l.left = p.new TreeNode(4);	l.right = p.new TreeNode(3);
-		r.left = p.new TreeNode(3);	r.right = p.new TreeNode(4);
-		System.out.println(p.maxDepth(root));
-
-	}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
+        int ans = 0;
+        if (root != null) {
+            q.offer(new Pair(root, 1));
+        }
+        while (! q.isEmpty()) {
+            Pair<TreeNode, Integer> cur = q.poll();
+            int curDepth = cur.getValue();
+            TreeNode curNode = cur.getKey();
+            ans = Math.max(ans, curDepth);
+            if (curNode.left != null) {
+                q.offer(new Pair(curNode.left, curDepth + 1));
+            }
+            if (curNode.right != null) {
+                q.offer(new Pair(curNode.right, curDepth + 1));
+            }
+        }
+        return ans;
+    }
 }
