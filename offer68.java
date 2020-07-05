0a39a6b64415288eb9cf4f4e8de66149286a3525
Byhuang1998 <Byhuang1998@gmail.com>
2020-07-05 22:15:48 +0800

    二叉树的最近共同祖先

---
+++ offer68.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftRes = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightRes = lowestCommonAncestor(root.right, p, q);
+        if (leftRes == null) {
+            return rightRes;
+        }
+        if (rightRes == null) {
+            return leftRes;
+        }
+        return root;
+    }
+}
